adc8532514b20ca588e7c7c52e0b846159bbeb15
brc <dbrckovi@gmail.com>
2025-02-17 23:30:54 +0100

    playing around

--- src/ConsoleFormsTest/Program.cs
+++ src/ConsoleFormsTest/Program.cs
@@ -7,63 +7,58 @@ class Program
 {
   static void Main(string[] args)
   {
-    Diagnostics();
+    StandardColors();
+    // Diagnostics();
     // Console.WriteLine("\u001b[38;2;255;0;0mThis is red text");
     // Console.WriteLine("\u001b[48;2;0;255;0mThis has a green background\u001b[0m");
     // Console.WriteLine("\u001b[38;2;255;255;0mYellow text on default background\u001b[0m");
   }
 
-  static void Diagnostics()
+  static void StandardColors()
   {
     Console.Clear();
-    Console.BackgroundColor = ConsoleColor.Black;
-    Console.ForegroundColor = ConsoleColor.White;
+    int x = 0;
+    int y = 0;
+
+    foreach (ConsoleColor fc in Enum.GetValues(typeof(ConsoleColor)))
+    {
+      Console.ForegroundColor = ConsoleColor.White;
+      Console.BackgroundColor = ConsoleColor.Black;
+      Console.SetCursorPosition(0, y);
+      Console.Write($"{fc.ToString()} ".PadRight(14,'─'));
     
-    PerformDiagnostic("BufferHeight, BufferWidth", new Action(() => { Console.Write($"{Console.BufferHeight}, {Console.BufferWidth}");} ));
-    PerformDiagnostic("CapsLock", new Action(() => { Console.Write($"{Console.CapsLock}");} ));
-    PerformDiagnostic("Beep", new Action(() => { Console.Beep(); } ));
-    PerformDiagnostic("CursorLeft", new Action(() => { Console.Write($"{Console.CursorLeft}");} ));
-    PerformDiagnostic("CursorTop", new Action(() => { Console.Write($"{Console.CursorTop}");} ));
-    PerformDiagnostic("CursorSize", new Action(() => { Console.Write($"{Console.CursorSize}");} ));
-    PerformDiagnostic("CursorVisible", new Action(() => { Console.Write($"{Console.CursorVisible}");} ));
-    PerformDiagnostic("GetCursorPosition", new Action(() => { Console.Write($"{Console.GetCursorPosition()}");} ));
-    PerformDiagnostic("InputEncoding", new Action(() => { Console.Write($"{Console.InputEncoding}");} ));
+      Console.SetCursorPosition (0, 18 + y);
+      Console.Write(fc.ToString());
 
+      for (int top = 17; top <= 17 + y; top ++)
+      {
+        Console.SetCursorPosition(15 + y, top);
+        Console.Write("│");
+      }
 
+      for (int left = fc.ToString().Length + 1; left <= 14 + y; left++)
+      {
+        Console.SetCursorPosition(left, 18 + y);
+        Console.Write("─");
+      }
+      Console.SetCursorPosition(15 + y, 18 + y);
+      Console.Write("┘");
 
+      Console.ForegroundColor = fc;
+      x = 0;
+      foreach (ConsoleColor bc in Enum.GetValues(typeof(ConsoleColor)))
+      {
+        Console.SetCursorPosition(x + 15, y);
+        Console.BackgroundColor = bc;
+        Console.Write("8");
+        x++;
+      }
 
-
-
-
-
-    // Console.WriteLine($"Caps lock: {Console.CapsLock}");
-    // Console.WriteLine($"BufferWidth: {Console.BackgroundColor}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"BufferWidth: {Console.BufferWidth}");
-    // Console.WriteLine($"Redirected: {Console.IsOutputRedirected}");
-    // Console.WriteLine("BEEP"); Console.Beep();
-  }
-
-  private static void PerformDiagnostic(string name, Action action)
-  {
-    Console.Write($"{name}: ");
-        
-    try
-    {
-      action.Invoke();
+      
+      y++;
     }
-    catch (Exception ex)
-    {
-      Console.Write(ex.Message);
-    }
+    Console.ResetColor();
+    Console.SetCursorPosition(0,40);
     Console.WriteLine();
   }
 }
